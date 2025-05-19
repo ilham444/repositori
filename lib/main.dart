8e58a5097fbd18f475814858d52affae5e9cac64
ilham444 <2206020@itg.ac.id>
2025-05-19 20:48:13 +0700

    Pesan commit kamu

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:newsapp/views/splash_screen.dart';
+
+//import 'package:newsapp/views/splash_screen.dart';
+import 'package:newsapp/views/utils/helper.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
+    return ScreenUtilInit(
+      designSize: Size(360, 800),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'tunduh',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: cPrimary),
+            useMaterial3: true,
+          ),
+          home: SplashScreen(),
+        );
+      },
+    );
+  }
+}
---
+++ lib/views/splash_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:newsapp/views/introduction_screen.dart';
+import 'package:newsapp/views/utils/helper.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future.delayed(Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => IntroductionScreen()),
+      );
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: cWhite,
+      body: Center(
+        child: Image.asset('assets/images/logo_and_title', width: 125),
+      ),
+    );
+  }
+}
